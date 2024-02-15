97364c122f33ae00655dda85afc9eaa3c7d64b35
sbx_user1051 <sbx_user1051@169.254.14.189>
2024-02-15 18:16:27 +0000

    Thu Feb 15 18:16:27 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -18,20 +18,20 @@ object CheckoutSolution {
                 'A' -> finalPrice += (value / 3) * 130 + (value % 3) * 50
                 'B' -> {
                     val amountOfE = mapSkuToQuantity['E']
-                    if(amountOfE != null && amountOfE / 2 >= value)
-                        finalPrice *= value * 30
-                    else
-                        finalPrice += (value / 2) * 45 + (value % 2) * 30
+                    val amountOfFreeBFromE = if(amountOfE == null) 0 else amountOfE / 2
+                    val remainingB = value - amountOfFreeBFromE
 
+                    if(amountOfE != null && remainingB > 0)
+                        finalPrice += (remainingB / 2) * 45 + (remainingB % 2) * 30
                 }
                 'C' -> finalPrice += 20 * value
                 'D' -> finalPrice += 15 * value
                 'E' -> {
                     finalPrice += 40 * value
-                    val amountOfB = mapSkuToQuantity['B']
+                    /*val amountOfB = mapSkuToQuantity['B']
                     if(amountOfB != null) {
                         finalPrice -= 30 * (value / 2)
-                    }
+                    }*/
                 }
                 else -> {
                     finalPrice = -1
