337714f7c34cc4d05c4b27194815fef0be843906
sbx_user1051 <sbx_user1051@169.254.14.189>
2024-02-15 17:47:08 +0000

    Thu Feb 15 17:47:08 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,6 +2,19 @@ package solutions.CHK
 
 object CheckoutSolution {
     fun checkout(skus: String): Int {
-        TODO("Solution not implemented")
+        val mapSkuToQuantity: MutableMap<Char, Int> = mutableMapOf()
+
+        skus.forEach {
+            run {
+                val quantity = mapSkuToQuantity[it]
+                if (quantity == null) mapSkuToQuantity[it] = 1
+                else mapSkuToQuantity[it] = quantity + 1
+            }
+        }
+
+        val sum = 0;
+        skus.forEach { entry -> {
+            if(entry.key < 'A' || key > 'D')
+        } }
     }
 }
